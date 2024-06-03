b6f8e1f2153f943181ded1a08db370f3ecf4ecc6
jocelyn-shannon-vf <168199191+jocelyn-shannon-vf@users.noreply.github.com>
2024-06-03 11:41:25 +0000

    openai RAG added

--- python/api/main.py
+++ python/api/main.py
@@ -3,6 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import FileResponse
 from starlette.staticfiles import StaticFiles
 
+import dotenv
+import os
+from openai import AzureOpenAI
+
 app = FastAPI()
 
 # Add CORS middleware
@@ -25,11 +29,29 @@ async def chat(request: Request):
     json = await request.json()
     print(json)
 
-    ############################
-    ### Add OpenAI code here ###
-    ############################
+    dotenv.load_dotenv()
 
-    return {"message": "Your message was: " + json["message"]}
+    import os
+    from openai import AzureOpenAI
+
+    client = AzureOpenAI(
+    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
+    api_version="2024-02-15-preview",
+    azure_deployment="turbogpt"
+    )
+
+    response = client.chat.completions.create(
+        model="turbogpt", # model = "deployment_name".
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": json["message"]}
+        ]
+    )
+
+    response.choices[0].message.content
+
+    return {"message": response.choices[0].message.content}
 
 # Image generattion API to be extended with OpenAI code
 @app.post("/generateImage")
