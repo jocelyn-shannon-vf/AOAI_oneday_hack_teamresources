f7a8c91edba48f91bde09309c38cd34cda1eca2d
nsantoss <nestorjose.santos@vodafone.com>
2024-06-04 14:26:16 +0200

    challenge 3

--- challenge_1/python/api/main.py
+++ challenge_1/python/api/main.py
@@ -95,38 +95,32 @@ async def generateImage(request: Request):
     response = chat_method(dalle_request)
 
     #new_request = request(json={"message",f"{dalle_request}"})
-
     #response=await chat(dalle_request)
-    ## Get Azure OpenAI Service settings
-    #dotenv.load_dotenv()
-    #api_base = os.getenv("AZURE_OPENAI_ENDPOINT"), 
-    #api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
-    #api_version="2024-02-15-preview",
-   # 
-    ## Get prompt for image to be generated
-    #prompt = json
 
-    ## Call the DALL-E model
-    #url = "{}openai/deployments/dalle3/images/generations?api-version={}".format(api_base, api_version)
-    #headers= { "api-key": api_key, "Content-Type": "application/json" }
-    #body = {
-    #    "prompt": prompt,
-    #    "n": 1,
-    #    "size": "1024x1024"
-    #}
-    #response = requests.post(url, headers=headers, json=body)
-    #print(response)
-    ## Get the revised prompt and image URL from the response
-    #revised_prompt = response.json()['data'][0]['revised_prompt']
-    #image_url = response.json()['data'][0]['url']
-
-    ## Display the URL for the generated image
-    #print(revised_prompt)
-    #print(image_url)
-
-    #return {"url": image_url}
-    #image_url = response.json()['data'][0]['url']
+    # Get Azure OpenAI Service settings
+    dotenv.load_dotenv()
+    api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
+    api_key=os.getenv("AZURE_OPENAI_API_KEY")
+    api_version="2024-02-15-preview"
+   
+    # Get prompt for image to be generated
+    prompt = response.choices[0].message.content
+    # Call the DALL-E model
+    url = "{}openai/deployments/dalle3/images/generations?api-version={}".format(api_base, api_version)
+    headers= { "api-key": api_key, "Content-Type": "application/json" }
+    body = {
+        "prompt": prompt,
+        "n": 1,
+        "size": "1024x1024"
+    }
+    response = requests.post(url, headers=headers, json=body)
     print(response)
-    return response
+    # Get the revised prompt and image URL from the response
+    revised_prompt = response.json()['data'][0]['revised_prompt']
+    image_url = response.json()['data'][0]['url']
+    # Display the URL for the generated image
+    print(revised_prompt)
+    print(image_url)
+    return {"url": image_url}
 
 app.mount("/", StaticFiles(directory="public"), name="ui")
